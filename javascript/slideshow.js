b5fdd0175489f32b9434a13153a4346c30d3908d
Rubberkat <roydejager@live.nl>
2016-06-29 11:01:50 +0200

    Changes to slideshow

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -3,17 +3,30 @@
  */
 
 
-var myIndex = 0;
+
+var myIndex = 1;
+
+function plusDivs(n) {
+    carousel(myIndex += n);
+}
+
 carousel();
 
-function carousel() {
+function carousel(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
+
+    //if (n > x.length) {slideIndex = 1}
+    //if (n < 1) {slideIndex = x.length}
+
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
+
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}
     x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    setTimeout(carousel, 2000); 
 }
+
+
